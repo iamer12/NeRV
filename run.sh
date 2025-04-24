b926786a077751ea524635c8536e892f898b88a4
Ihab Amer <iamer78@gmail.com>
2025-04-24 14:33:53 -0400

    slight modifications to bash file

--- run.sh
+++ run.sh
@@ -36,7 +36,7 @@ python train_nerv.py -e 100   --lower-width 96 --num-blocks 1 --dataset bunny --
     --outf dbg --embed 1.25_40 --stem_dim_num 512_1  --reduction 2  --fc_hw_dim 9_16_26 --expansion 1  \
     --single_res --loss Fusion6   --warmup 0. --lr_type cosine  --strides 5 2 2 2 2  --conv_type conv \
     -b 1  --lr 0.0005 --norm none --suffix 107  --act swish \
-    --weight checkpoints/nerv_S_pruned.pth --prune_ratio 0.4  --eval_only --quant_bit 5 --quant_axis 0 --num_frames 2 --num_prec_layers 2 --quant_bit_enh 5 5 5 \
+    --weight checkpoints/nerv_S_pruned.pth --prune_ratio 0.4  --eval_only --quant_bit 4 --quant_axis 0 --num_frames 2 --num_prec_layers 2 --quant_bit_enh 4 4 4 \
     --qmode mdlns --mdlns_second_base 1000 --mdlns_second_base_exp_num_bits 2 2 2 2\
     --mdlns_sweep_start 0.1 --mdlns_sweep_end 5.0 --mdlns_sweep_step 0.1
 
@@ -51,7 +51,7 @@ python train_nerv.py -e 100   --lower-width 96 --num-blocks 1 --dataset bunny --
 #     --outf dbg --embed 1.25_40 --stem_dim_num 512_1  --reduction 2  --fc_hw_dim 9_16_26 --expansion 1  \
 #     --single_res --loss Fusion6   --warmup 0. --lr_type cosine  --strides 5 2 2 2 2  --conv_type conv \
 #     -b 1  --lr 0.0005 --norm none --suffix 107  --act swish \
-#     --weight checkpoints/nerv_S_pruned.pth --prune_ratio 0.4  --eval_only --quant_bit 5 --num_frames 2 --num_prec_layers 1 --quant_bit_enh 5 5 5
+#     --weight checkpoints/nerv_S_pruned.pth --prune_ratio 0.4  --eval_only --quant_bit 4 --num_frames 2 --num_prec_layers 2 --quant_bit_enh 4 4 4
 
 
 
